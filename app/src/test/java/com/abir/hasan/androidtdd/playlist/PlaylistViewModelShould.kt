9be5b352af7a3aa5eb05fcecee740844b8f77af2
Abir Hasan Shawon <abirhasan89@gmail.com>
2022-06-30 16:18:17 +0200

    PlaylistViewModel Unit tests written

--- app/src/test/java/com/abir/hasan/androidtdd/playlist/PlaylistViewModelShould.kt
+++ app/src/test/java/com/abir/hasan/androidtdd/playlist/PlaylistViewModelShould.kt
@@ -12,7 +12,6 @@ import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
-import org.junit.Before
 import org.junit.Test
 import petros.efthymiou.groovy.utils.getValueForTest
 
@@ -22,27 +21,39 @@ class PlaylistViewModelShould : BaseUnitTest() {
     private val repository: PlaylistRepository = mock()
     private val playlists = mock<List<Playlist>>()
     private val expected = Result.success(playlists)
-    private lateinit var viewModel: PlaylistViewModel
-
-    @Before
-    fun initializeTest() = runTest {
-        whenever(repository.getPlaylists()).thenReturn(
-            flow {
-                emit(expected)
-            }
-        )
-        viewModel = PlaylistViewModel(repository)
-    }
+    private val exception = RuntimeException("Something went wrong")
 
     @Test
     fun getPlaylistsFroRepository() = runTest {
+        val viewModel = mockSuccessfulCase()
         viewModel.playlists.getValueForTest()
         verify(repository, times(1)).getPlaylists()
     }
 
     @Test
     fun emitsPlaylistsFromRepository() = runTest {
+        val viewModel = mockSuccessfulCase()
         assertEquals(expected, viewModel.playlists.getValueForTest())
+    }
+
+    private suspend fun mockSuccessfulCase(): PlaylistViewModel {
+        whenever(repository.getPlaylists()).thenReturn(
+            flow {
+                emit(expected)
+            }
+        )
+        return PlaylistViewModel(repository)
+    }
+
+    @Test
+    fun emitErrorWhenReceivedError() = runTest {
+        whenever(repository.getPlaylists()).thenReturn(
+            flow {
+                emit(Result.failure<List<Playlist>>(exception))
+            }
+        )
+        val viewModel = PlaylistViewModel(repository)
+        assertEquals(exception, viewModel.playlists.getValueForTest()!!.exceptionOrNull())
     }
 
 }
