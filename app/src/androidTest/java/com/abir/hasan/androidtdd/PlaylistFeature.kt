361c1cf68928c4f1fab43eede2a55971d9a005e1
Abir Hasan Shawon <abirhasan89@gmail.com>
2022-07-02 00:26:03 +0200

    Removing thread.sleep and introduced idling

--- app/src/androidTest/java/com/abir/hasan/androidtdd/PlaylistFeature.kt
+++ app/src/androidTest/java/com/abir/hasan/androidtdd/PlaylistFeature.kt
@@ -1,28 +1,22 @@
 package com.abir.hasan.androidtdd
 
-import android.view.View
-import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.abir.hasan.androidtdd.ui.MainActivity
+import com.abir.hasan.androidtdd.ui.playlist.di.idlingResource
 import com.adevinta.android.barista.assertion.BaristaRecyclerViewAssertions.assertRecyclerViewItemCount
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed
 import com.adevinta.android.barista.internal.matcher.DrawableMatcher.Companion.withDrawable
 import org.hamcrest.CoreMatchers.allOf
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 
-@RunWith(AndroidJUnit4::class)
-class PlaylistFeature {
+class PlaylistFeature : BaseUITest() {
 
     // ActivityTestRule is deprecated
     val mActivityRule = ActivityScenarioRule(MainActivity::class.java)
@@ -35,9 +29,6 @@ class PlaylistFeature {
 
     @Test
     fun displaysListOfPlaylists() {
-
-        Thread.sleep(4000) // TODO - Refactor this later with idling
-
         assertRecyclerViewItemCount(R.id.rvPlaylist, 10)
 
         onView(
@@ -68,38 +59,20 @@ class PlaylistFeature {
             .check(matches(isDisplayed()))
     }
 
-    fun nthChildOf(parentMatcher: Matcher<View>, childPosition: Int): Matcher<View> {
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("position $childPosition of parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                if (view.parent !is ViewGroup) return false
-                val parent = view.parent as ViewGroup
-
-                return (parentMatcher.matches(parent)
-                        && parent.childCount > childPosition
-                        && parent.getChildAt(childPosition) == view)
-            }
-        }
-    }
-
     @Test
     fun displaysLoaderWhileFetchingThePlaylist() {
+        // Resuming the UI thread
+        IdlingRegistry.getInstance().unregister(idlingResource)
         assertDisplayed(R.id.loader)
     }
 
     @Test
     fun hidesLoader() {
-        Thread.sleep(4005)
         assertNotDisplayed(R.id.loader)
     }
 
     @Test
     fun displaysRockImageForRockListItems() {
-        Thread.sleep(4005)
         onView(
             allOf(
                 withId(R.id.ivPlaylistImage),
--- app/src/main/java/com/abir/hasan/androidtdd/ui/playlist/di/PlaylistModule.kt
+++ app/src/main/java/com/abir/hasan/androidtdd/ui/playlist/di/PlaylistModule.kt
@@ -1,6 +1,8 @@
 package com.abir.hasan.androidtdd.ui.playlist.di
 
+import androidx.test.espresso.IdlingResource
 import com.abir.hasan.androidtdd.data.remote.PlaylistAPI
+import com.jakewharton.espresso.OkHttp3IdlingResource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -8,6 +10,9 @@ import dagger.hilt.android.components.FragmentComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
+val client = OkHttpClient()
+val idlingResource: IdlingResource = OkHttp3IdlingResource.create("okhttp", client)
 
 @Module
 @InstallIn(FragmentComponent::class)
@@ -17,9 +22,10 @@ class PlaylistModule {
     fun provideRetrofit() = Retrofit.Builder()
         //.baseUrl("http://127.0.0.1:3000/") // Check local IP
         .baseUrl("http://10.0.2.2:3000/") // Check local IP
-        .client(OkHttpClient())
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
 
     @Provides
     fun playlistAPI(retrofit: Retrofit): PlaylistAPI {
